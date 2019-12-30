ed56eaefa0deeb4b8383c23a85ec6ad8af6429fd
durgaprasad <kolli.durgaprasad8000@gmail.com>
2019-12-30 20:00:07 +0530

    add Branch Offices

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { Routes, RouterModule } from '@angular/router';
 import {LoginComponent} from './login/login.component';
 import {HomeComponent} from './home/home.component';
 import {AuthGuard} from './_helpers/auth.guard';
+import {BranchofficesComponent} from './branchoffices/branchoffices.component';
 
 
 const routes: Routes = [
   {path: '', redirectTo: '', pathMatch: 'full',  component: HomeComponent, canActivate: [AuthGuard] },
   {path: 'login', component: LoginComponent},
+  {path: 'branchOffices', component: BranchofficesComponent},
 
   // otherwise redirect to home
   { path: '**', component: HomeComponent }
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,13 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 import {ApiServiceService} from '../api/api-service.service';
+import {NgbDateAdapter, NgbDateNativeAdapter} from '@ng-bootstrap/ng-bootstrap';
 
 declare var $: any;
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
+  providers: [{provide: NgbDateAdapter, useClass: NgbDateNativeAdapter}]
 })
 export class HomeComponent implements OnInit {
+  dashBoardDate = new Date();
 
   constructor(private apiService: ApiServiceService) {}
 
